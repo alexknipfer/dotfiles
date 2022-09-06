4e1d91c66c06fb6b239b4ba678df92e8f1baa304
Alex <alexanderknipfer@gmail.com>
2022-09-06 12:04:15 -0400

    Switch to mason

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -76,8 +76,9 @@ return packer.startup(function(use)
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
