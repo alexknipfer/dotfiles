f23df5e26f231c6f72db0c630f439f5b43992680
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-04-04 20:45:15 -0400

    Add multicursor

--- nvim/init.lua
+++ nvim/init.lua
@@ -405,4 +405,21 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 		},
 	},
+	{
+		"smoka7/multicursors.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"smoka7/hydra.nvim",
+		},
+		opts = {},
+		cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+		keys = {
+			{
+				mode = { "v", "n" },
+				"<Leader>c",
+				"<cmd>MCstart<cr>",
+				desc = "Create a selection for selected text or word under the cursor",
+			},
+		},
+	},
 })
