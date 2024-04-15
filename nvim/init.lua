162cc0f3599b501b216296f839ff9e765eafc7ff
Alex <alexanderknipfer@gmail.com>
2024-04-15 09:48:30 -0400

    Add telescope undo to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -110,6 +110,29 @@ require("lazy").setup({
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 	{
+		"debugloop/telescope-undo.nvim",
+		dependencies = { -- note how they're inverted to above example
+			{
+				"nvim-telescope/telescope.nvim",
+				dependencies = { "nvim-lua/plenary.nvim" },
+			},
+		},
+		keys = {
+			{
+				"<leader>u",
+				"<cmd>Telescope undo<cr>",
+				desc = "Undo History",
+			},
+		},
+		config = function(_, opts)
+			-- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+			-- configs for us. We won't use data, as everything is in it's own namespace (telescope
+			-- defaults, as well as each extension).
+			require("telescope").setup(opts)
+			require("telescope").load_extension("undo")
+		end,
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		event = "VeryLazy",
 		branch = "0.1.x",
