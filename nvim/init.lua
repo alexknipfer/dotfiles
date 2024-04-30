7c241f5aa41e323cc2d2b3abe97edc99044c9747
Alex <alexanderknipfer@gmail.com>
2024-04-30 10:19:59 -0400

    Add javascriptreact formatter to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -297,6 +297,7 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
 				typescript = { { "prettierd", "prettier" } },
 				typescriptreact = { { "prettierd", "prettier" } },
 			},
