2b2906e240aa331448dff3dd4c88c9e3a725c05f
Alex <alexanderknipfer@gmail.com>
2024-08-19 11:13:36 -0400

    Update trouble config

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,7 +108,7 @@ require("lazy").setup({
 				{ "<leader>m", "<cmd>Mason<CR>", desc = "Mason LSP" },
 				{ "<leader>p", "<cmd>Lazy<CR>", desc = "Plugin Manager" },
 				{ "<leader>q", "<cmd>wqall!<CR>", desc = "Quit" },
-				{ "<leader>t", "<cmd>TroubleToggle<CR>", desc = "Trouble" },
+				{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
 				{ "<leader>w", "<cmd>w!<CR>", desc = "Save" },
 			})
 		end,
@@ -116,7 +116,40 @@ require("lazy").setup({
 	-- Trouble
 	{
 		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		cmd = "Trouble",
+		opts = {},
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
 	},
 	{
 		"debugloop/telescope-undo.nvim",
