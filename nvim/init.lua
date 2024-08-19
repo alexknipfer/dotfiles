0d1959af7a8acffee7a3194bdee8435076e052b5
Alex <alexanderknipfer@gmail.com>
2024-08-19 14:03:42 -0400

    Add web devicons to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -188,6 +188,7 @@ require("lazy").setup({
 				end,
 			},
 			{ "nvim-telescope/telescope-ui-select.nvim" },
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 		},
 		config = function()
 			require("telescope").setup({
