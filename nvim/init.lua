8bf84074695ff395c216b05eabc7577a6e66fd8b
Alex <alexanderknipfer@gmail.com>
2024-09-30 10:10:08 -0400

    Use empty objects to fix prettierd not formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -343,17 +343,18 @@ require("lazy").setup({
 	{
 		"stevearc/conform.nvim",
 		opts = {
-			notify_on_error = false,
+			notify_on_error = true,
+			log_level = vim.log.levels.DEBUG,
 			format_on_save = {
 				timeout_ms = 500,
 				lsp_fallback = true,
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
+				javascript = {},
+				javascriptreact = {},
+				typescript = {},
+				typescriptreact = {},
 			},
 		},
 	},
