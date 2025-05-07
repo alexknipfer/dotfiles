38474d9b19ad3694b3f83fffd8cbeb85d0553240
Alex <alexanderknipfer@gmail.com>
2025-05-06 21:43:17 -0400

    nvim treat <CR> normal enter if nothing is selected for completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -357,6 +357,19 @@ later(function()
 		},
 	})
 
+	_G.cr_action = function()
+		-- If there is selected item in popup, accept it with <C-y>
+		if vim.fn.complete_info()["selected"] ~= -1 then
+			return "\25"
+		end
+		-- Fall back to plain `<CR>`. You might want to customize according
+		-- to other plugins. For example if 'mini.pairs' is set up, replace
+		-- next line with `return MiniPairs.cr()`
+		return "\r"
+	end
+
+	vim.keymap.set("i", "<CR>", "v:lua.cr_action()", { expr = true })
+
 	-- vim.o.completeopt = "menuone,noinsert"
 end)
 
