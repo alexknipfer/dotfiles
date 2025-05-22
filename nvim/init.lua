41848603f82ee85be60e7115304abe8e841892d3
Alex <alexanderknipfer@gmail.com>
2025-05-22 17:03:04 -0400

    prevent tab from selecting copilot suggestion

--- nvim/init.lua
+++ nvim/init.lua
@@ -119,6 +119,7 @@ now(function()
 	vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 	vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 	vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+	vim.g.copilot_no_tab_map = true
 end)
 
 -- Keymaps
@@ -564,7 +565,6 @@ now(function()
 		expr = true,
 		replace_keycodes = false,
 	})
-	vim.g.copilot_no_tab_map = true
 end)
 
 -- LSP
