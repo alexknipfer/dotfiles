001babd8d063cf1f3aabf4a155f2efadc5c14ba0
Alex Knipfer <alexanderknipfer@gmail.com>
2026-04-11 22:26:58 -0400

    neovim: add biome-check to conform

--- nvim/init.lua
+++ nvim/init.lua
@@ -607,9 +607,9 @@ later(function()
 		formatters_by_ft = {
 			lua = { "stylua" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "biome-check", "prettierd", "prettier", stop_after_first = true },
 			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "biome-check", "prettierd", "prettier", stop_after_first = true },
 			css = { "prettierd", "prettier", stop_after_first = true },
 			html = { "prettierd", "prettier", stop_after_first = true },
 			json = { "prettierd", "prettier", stop_after_first = true },
